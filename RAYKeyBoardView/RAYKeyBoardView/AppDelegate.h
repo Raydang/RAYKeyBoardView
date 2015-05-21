68b35d84ab5afa8e06852074820bfecf2e0bef03
raydang <17151328@qq.com>
2015-05-21 10:26:04 +0800

    Create project.

---
+++ RAYKeyBoardView/RAYKeyBoardView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAYKeyBoardView
+//
+//  Created by richerpay on 15/5/21.
+//  Copyright (c) 2015年 richerpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RAYKeyBoardView/RAYKeyBoardView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RAYKeyBoardView
+//
+//  Created by richerpay on 15/5/21.
+//  Copyright (c) 2015年 richerpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
